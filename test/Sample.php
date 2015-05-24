ec3b5c48ae070d725e5c3772fcf6f036d4853955
taisa_007 <g5.taisa007@gmail.com>
2015-05-24 14:48:04 +0900

    initial

---
+++ test/Sample.php
@@ -0,0 +1,12 @@
+<?php
+
+class Sample extends PHPUnit_Framework_TestCase {
+
+    /** @test **/
+    public function testPhp() {
+        $text = 'sample';
+        $result = 'sample';
+        $this->assertThat($result, $this->equalTo($text));
+    }
+
+}
